3d3723044c8dfc1a811faf6ae5a1a2d6953d22b1
woody1983 <unix1983@gmail.com>
2012-10-21 09:58:15 +0800

    Initial commit phxer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phxer::Application.config.secret_token = '514b13b8c88c072e5619096cc4e13b7e2b6ebca2579da63bac2dded0b1d2b6719d289e0694d86649f10c4656dd136ff6cd71c1876beb50b52b712e5574502393'
